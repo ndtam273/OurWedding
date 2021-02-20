4aed59d8c29f83c1be39a8235e968e50a4cec437
Hieu Tran T <t2hieu91@gmail.com>
2021-02-20 18:10:17 +0700

    first code

--- lib/screens/CountDownScreen/components/body.dart
+++ lib/screens/CountDownScreen/components/body.dart
@@ -3,8 +3,21 @@ import 'package:flutter/material.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Hello world!"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            color: Colors.amber,
+            child: Center(
+              child: Text(
+                'Thoi gian',
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
