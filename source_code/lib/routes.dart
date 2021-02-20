16d363e20bba68aaaa401614cd25e7edd1aecb06
ndtam273 <ndtam.273@gmail.com>
2021-02-20 10:29:35 +0700

    init project

---
+++ source_code/lib/routes.dart
@@ -0,0 +1,26 @@
+// import 'package:e_commerce_flutter/screens/cart/cart_screen.dart';
+// import 'package:e_commerce_flutter/screens/complete_profile/complete_profile_screen.dart';
+// import 'package:e_commerce_flutter/screens/details/details_screen.dart';
+// import 'package:e_commerce_flutter/screens/forgot_password/forgot_password_screen.dart';
+// import 'package:e_commerce_flutter/screens/home/home_screen.dart';
+// import 'package:e_commerce_flutter/screens/login_success/login_success_screen.dart';
+// import 'package:e_commerce_flutter/screens/otp/otp_screen.dart';
+// import 'package:e_commerce_flutter/screens/profile/profile_screen.dart';
+// import 'package:e_commerce_flutter/screens/sign_in/sign_in_screen.dart';
+// import 'package:e_commerce_flutter/screens/sign_up/sign_up_screen.dart';
+// import 'package:e_commerce_flutter/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  // SplashScreen.routeName: (context) => SplashScreen(),
+  // SignInScreen.routeName: (context) => SignInScreen(),
+  // ForgotPassword.routeName: (context) => ForgotPassword(),
+  // LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  // SignUpScreen.routeName: (context) => SignUpScreen(),
+  // CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  // OtpScreen.routeName: (context) => OtpScreen(),
+  // HomeScreen.routeName: (context) => HomeScreen(),
+  // DetailsScreen.routeName: (context) => DetailsScreen(),
+  // CartScreen.routeName: (context) => CartScreen(),
+  // ProfileScreen.routeName: (context) => ProfileScreen()
+};
