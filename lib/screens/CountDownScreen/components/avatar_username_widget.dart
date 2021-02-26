aecbc1f1fd6407a103e55e8b5c19bacd1e3891ab
Trung Hieu <hieutt@d-soft.com.vn>
2021-02-26 11:48:17 +0700

    Finish ui for screen

---
+++ lib/screens/CountDownScreen/components/avatar_username_widget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AvatarUserNameWidget extends StatelessWidget {
+  final String username;
+
+  AvatarUserNameWidget({
+    this.username,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {},
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.white,
+              child: Text('Avatar'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                username,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
